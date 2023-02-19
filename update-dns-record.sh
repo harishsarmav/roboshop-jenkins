b4332cc0aec27afc4cb573a2dcacd40107315879
Hari <harishsarma.v@gmail.com>
2023-02-19 19:15:11 +0530

    updating roboshop-jenkins

--- update-dns-record.sh
+++ update-dns-record.sh
@@ -14,4 +14,4 @@ echo '
 }' | sed -e "s/IPADDRESS/${IP}/" >/tmp/jenkins.json
 
 ZONE_ID="Z09654941FERAO4Q3504K"
-aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/jenkins.json | jq.
+aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/jenkins.json | jq .
